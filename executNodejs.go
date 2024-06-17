0d3ff39856bb0e6995c0b231bf1a4ae0a656efab
ericNKS <ek.silva.santos@gmail.com>
2024-06-17 15:01:16 -0300

    :bug: fix: Solving bug that a new cmd windows no open

--- executNodejs.go
+++ executNodejs.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ExecuteNodeJs(dir string) {
-	cmd := exec.Command("cmd", "/c", "start", "node", dir)
+	cmd := exec.Command("cmd", "/c", "start", "cmd", "/k", "node", dir)
 	if cmd.Run() != nil {
 		fmt.Printf("Erro ao iniciar o comando: \n")
 		panic(cmd)
