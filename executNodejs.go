a559caca4f7c25e94715846a17763f3ec012ea8d
ericNKS <ek.silva.santos@gmail.com>
2024-06-18 17:35:24 -0300

    fix: Now the prompt will be in the directory in a new window

--- executNodejs.go
+++ executNodejs.go
@@ -6,7 +6,8 @@ import (
 )
 
 func ExecuteNodeJs(dir string) {
-	cmd := exec.Command("cmd", "/c", "start", "cmd", "/k", "node", dir)
+	//cmd := exec.Command("cmd", "/c", "start", "cmd", "/k", "node", dir)
+	cmd := exec.Command("cmd", "/c", "start", "cmd", "/k", "cd /d "+dir+" && node index")
 	if cmd.Run() != nil {
 		fmt.Printf("Erro ao iniciar o comando: \n")
 		panic(cmd)
